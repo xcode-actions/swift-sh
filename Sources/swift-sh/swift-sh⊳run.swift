c3dfaa46d0fe6228259ee6745a7c3ff2eda63287
Frizlab <fload@me.com>
2024-05-09 00:42:00 +0200

    Use the new swift-process-invocation for sending the script data when using -c

--- Sources/swift-sh/swift-sh⊳run.swift
+++ Sources/swift-sh/swift-sh⊳run.swift
@@ -108,66 +108,14 @@ struct Run : AsyncParsableCommand {
 			return ret
 		}()
 		
-		let stdinForSwift: FileDescriptor
-		if let data = scriptData {
-			let pipe = try ProcessInvocation.unownedPipe()
-			stdinForSwift = pipe.fdRead
-			if data.0.count > 0 {
-				let writtenRef = IntRef(value: 0)
-				let fhWrite = FileHandle(fileDescriptor: pipe.fdWrite.rawValue)
-				fhWrite.writeabilityHandler = { fh in
-					data.0.withUnsafeBytes{ (bytes: UnsafeRawBufferPointer) in
-						let writtenTotal: Int
-						let writtenBefore = writtenRef.value
-						
-						let writtenNow = {
-							var ret: Int
-							repeat {
-								ret = write(fh.fileDescriptor, bytes.baseAddress!.advanced(by: writtenBefore), bytes.count - writtenBefore)
-							} while ret == -1 && errno == EINTR
-							return ret
-						}()
-						if writtenNow >= 0 {
-							writtenTotal = writtenNow + writtenBefore
-							writtenRef.value = writtenTotal
-						} else {
-							if [EAGAIN, EWOULDBLOCK].contains(errno) {
-								/* We ignore the write error and let the writeabilityHandler call us back (let’s hope it will!). */
-								writtenTotal = writtenBefore
-							} else {
-								writtenTotal = -1
-								logger.warning("Failed write end of fd for pipe to swift.", metadata: ["errno": "\(errno)", "errno-str": "\(Errno(rawValue: errno).localizedDescription)"])
-							}
-						}
-						
-						if bytes.count - writtenTotal <= 0 || writtenTotal == -1 {
-							fhWrite.writeabilityHandler = nil
-							if close(fh.fileDescriptor) == -1 {
-								logger.warning("Failed closing write end of fd for pipe to swift.", metadata: ["errno": "\(errno)", "errno-str": "\(Errno(rawValue: errno).localizedDescription)"])
-							}
-						}
-					}
-				}
-			} else {
-				try pipe.fdWrite.close()
-			}
-		} else {
-			stdinForSwift = .standardInput
-		}
-		
 		let allArgs = swiftArgs + [scriptPathForSwift] + scriptArguments
 		logger.trace("Running script.", metadata: ["invocation": .array((["swift"] + allArgs).map{ "\($0)" })])
 		let (exitCode, terminationReason) = try await ProcessInvocation(
 			swiftPath, args: allArgs, usePATH: true,
-			stdin: stdinForSwift, stdoutRedirect: .none, stderrRedirect: .none,
+			stdinRedirect: scriptData.flatMap{ .send($0.0) } ?? .none(), stdoutRedirect: .none, stderrRedirect: .none,
 			signalHandling: { .mapForChild(for: $0, with: [.interrupt: .terminated]/* Swift eats the interrupts for some reasons… */) },
 			expectedTerminations: .some(nil)
 		).invokeAndStreamOutput(checkValidTerminations: false/* Doesn’t matter, all terminations are valid. */, outputHandler: { _, _, _ in })
-		
-		if stdinForSwift != .standardInput {
-			do    {try stdinForSwift.close()}
-			catch {logger.warning("Failed closing read end of fd for pipe to swift.", metadata: ["error": "\(error)"])}
-		}
 		
 		if terminationReason == .uncaughtSignal {
 			logger.info("Script received uncaught signal.")
@@ -182,17 +130,6 @@ extension Run {
 	
 	fileprivate var logger: Logger {
 		globalOptions.logger
-	}
-	
-}
-
-
-private class IntRef {
-	
-	var value: Int
-	
-	init(value: Int) {
-		self.value = value
 	}
 	
 }
