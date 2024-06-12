3917784be29684095fd57c315fc8b2709c2b2269
Frizlab <fload@me.com>
2024-06-12 23:57:16 +0200

    Do not set the fg pgid to child process by default

--- Sources/swift-sh/swift-sh⊳run.swift
+++ Sources/swift-sh/swift-sh⊳run.swift
@@ -19,6 +19,13 @@ struct Run : AsyncParsableCommand {
 	@OptionGroup
 	var runOptions: BuildAndRunOptions
 	
+	@Flag(name: .customLong("set-fg-pgid-for-stdin"), inversion: .prefixedNo, help: """
+		Set this to set the foreground group ID associated with the controlling terminal to “your” stdin.
+		What this means is you’ll receive text typed in the Terminal when reading stdin if the flag is set, otherwise you won’t.
+		This is disabled by default because this will _also_ send you the signals, and Swift ignores the Ctrl-C signal for some reasons, which is not convenient.
+		""")
+	var setFgPgID = false
+	
 	@Argument(parsing: .captureForPassthrough)
 	var scriptArguments: [String] = []
 	
@@ -30,7 +37,7 @@ struct Run : AsyncParsableCommand {
 		logger.trace("Running script.", metadata: ["invocation": .array((["swift"] + allArgs).map{ "\($0)" })])
 		let (exitCode, terminationReason) = try await ProcessInvocation(
 			swiftPath, args: allArgs, usePATH: true,
-			stdinRedirect: stdinData.flatMap{ .send($0) } ?? .none(), stdoutRedirect: .none, stderrRedirect: .none,
+			stdinRedirect: stdinData.flatMap{ .send($0) } ?? .none(setFgPgID: setFgPgID), stdoutRedirect: .none, stderrRedirect: .none,
 			signalHandling: { .mapForChild(for: $0, with: [.interrupt: .terminated]/* Swift eats the interrupts for some reasons… */) },
 			expectedTerminations: .some(nil)
 		).invokeAndStreamOutput(checkValidTerminations: false/* Doesn’t matter, all terminations are valid. */, outputHandler: { _, _, _ in })
