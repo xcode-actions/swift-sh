2088b1d06e6c01466335e0b6102dba14b9522189
Frizlab <fload@me.com>
2026-01-08 01:38:41 +0100

    Add a new convenience method to change the cwd as well as some other global variables

---
+++ Sources/SwiftSH_Helpers/UsefulGlobals.swift
@@ -0,0 +1,80 @@
+import Foundation
+
+import ProcessInvocation
+import SystemPackage
+import UnwrapOrThrow
+
+
+
+public let fm = FileManager.default
+
+/* The path to the script.
+ * We assume arguments has its first argument properly set to the script’s path. */
+public let scriptPath: FilePath = .init(CommandLine.arguments[0])
+public let scriptFolderPath: FilePath = scriptPath.removingLastComponent()
+
+public let scriptURL: URL = .init(filePath: scriptPath.string, directoryHint: .notDirectory)
+public let scriptFolderURL: URL = .init(filePath: scriptFolderPath.string, directoryHint: .isDirectory)
+
+
+public enum PathBase : Sendable {
+	
+	case scriptFolder
+	case scriptRepoRoot
+	
+}
+
+public func changeCurrentDirectoryPath(_ pathBase: PathBase? = nil, _ relativePath: FilePath? = nil) throws {
+	let pathBase: FilePath = try {
+		switch pathBase {
+			case .scriptFolder?:
+				return scriptFolderPath
+				
+			case .scriptRepoRoot?:
+				var errOutput = ""
+				var repoRootResult: Result<String, Error>?
+				let pi = ProcessInvocation("git", "rev-parse", "--show-toplevel", workingDirectory: scriptFolderURL)
+				let (process, dispatchGroup) = try pi.invoke{ result, signalEndOfInterestForStream, process in
+					do {
+						let lineWithSource = try result.get()
+						guard lineWithSource.fd == .standardOutput else {
+							errOutput += lineWithSource.strLineOrHex() + "\n"
+							return
+						}
+						if repoRootResult != nil {
+							/* Only an empty line is acceptable here: git should output exactly a single line
+							 *  (unless one of the path component contains a newline, but let’s not go there…). */
+							guard lineWithSource.line.isEmpty, lineWithSource.eol.isEmpty else {
+								throw MessageError("Cannot determine the root of the repository: git output was not a single line.")
+							}
+						} else {
+							let line = try lineWithSource.strLine()
+							repoRootResult = .success(line)
+						}
+					} catch {
+						repoRootResult = .failure(error)
+					}
+				}
+				process.waitUntilExit()
+				dispatchGroup.wait()
+				
+				let (exitStatus, exitReason) = (process.terminationStatus, process.terminationReason)
+				guard exitStatus == 0, exitReason == .exit else {
+					throw MessageError("Cannot determine the root of the repository: git failed. git stderr:\n\(errOutput)")
+				}
+				
+				return FilePath(try repoRootResult?.get() ?! MessageError("Cannot determine the root of the repository: git did not output anything."))
+				
+			case nil:
+				return "."
+		}
+	}()
+	
+	let newPath: FilePath =
+		if let relativePath {pathBase.pushing(relativePath)}
+		else                {pathBase}
+	
+	guard fm.changeCurrentDirectoryPath(newPath.string) else {
+		throw MessageError("Failed setting current directory path to \(newPath.string).")
+	}
+}
