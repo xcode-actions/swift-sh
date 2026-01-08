0e1a1ea2a9993c11df97e9f1383d4d7bf3b66c9b
Frizlab <fload@me.com>
2026-01-08 17:09:29 +0100

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
 	],
 	dependencies: [
 		.package(url: "https://github.com/apple/swift-argument-parser.git",            from: "1.2.0"),
-		.package(url: "https://github.com/apple/swift-crypto.git",                     "1.0.0" ..< "4.0.0"),
+		.package(url: "https://github.com/apple/swift-crypto.git",                     "1.0.0" ..< "5.0.0"),
 		.package(url: "https://github.com/apple/swift-log.git",                        from: "1.8.0"),
 		.package(url: "https://github.com/apple/swift-system.git",                     from: "1.0.0"), /* We’re aware of the existence of System on macOS. After some thinking/research, we decided to agree with <https://forums.swift.org/t/50719/5>. */
 		.package(url: "https://github.com/Frizlab/InlineObjectConfig.git",             from: "1.0.0"),
